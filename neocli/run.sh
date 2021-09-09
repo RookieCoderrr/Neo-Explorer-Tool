fef3d5f16aa5cf93b3a34d6afdfe8f44ebf06937
RookieCoderrr <zilie.imtatlantique@gmail.com>
2021-09-09 14:01:18 +0800

    update neo-cli

--- neocli/run.sh
+++ neocli/run.sh
@@ -2,4 +2,4 @@
 
 docker build --no-cache -t neo-private .
 docker rm  --force neo-rc2-private
-docker run -dit --name=neo-rc2-private -v $PWD/SmartContract:/neo-cli/SmartContract --link mongodb1_test --net network -e INIT_ADDRESS=NhZTTsKwdNQXU9FwadjA8hnU156a8QB5kz  -p 20332-20334:20332-20334 neo-private
+docker run -dit --name=neo-rc2-private -v $PWD/SmartContract:/neo-cli/SmartContract --link mongodb1_test --net network   -p 20332-20334:20332-20334 neo-private
