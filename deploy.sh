99e3c298a7a95082cde9806d092e8f7598323b0e
ZILIE QIN <86407596+RookieCoderrr@users.noreply.github.com>
2022-06-22 10:08:45 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,26 +4,19 @@ echo 'Shut down existed neo-cli================'
 
 docker stop neo-rc2-private
 
-echo 'done'
-
 echo 'Start Neofura module=================='
 
 cd neo3fura
 
 sh start.sh TEST
 
-echo 'Neofura Start Success'
-
 cd ..
-
 
 echo 'Start NeoExplorer Module=================='
 
 cd NeoExplorer
 
 sh run.sh
-
-echo 'Neocli NeoExplorer Success'
 
 cd ..
 
@@ -43,8 +36,6 @@ docker exec -it mongodb1_test bash createNewOnlineDb.sh
 
 docker exec -it mongo_test bash createNewLocalDb.sh
 
-echo 'Done'
-
 cd ..
 
 echo 'Start Neocli Module=================='
@@ -52,7 +43,5 @@ echo 'Start Neocli Module=================='
 cd neocli
 
 sh run.sh
-
-echo 'Neocli Start Success'
 
 cd ..
